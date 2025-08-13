039bb1c9ad95b1fdbdeefdd53e102c39971e2d5a
Vu Binh <22520129@gm.uit.edu.vn>
2025-08-13 20:55:51 +0700

    ProductController: Create some API to get all product and get product by id

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ProductController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            var products = context.Products.ToList();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var product = context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+    }
+}
