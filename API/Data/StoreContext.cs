ce455076eed3b05c327c1222b7ff421ef37bde63
Vu Binh <22520129@gm.uit.edu.vn>
2025-08-13 20:36:50 +0700

    initialize project

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace API.Data
+{
+	public class StoreContext : DbContext
+	{
+		public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+		{
+		}
+
+		// Define DbSets for your entities here
+		public DbSet<Product> Products { get; set; }
+	}
+}
